e138f99d8e04a1e8a91c4025207f0181ec62a012
Gagandeep Kang <gskang@bu.edu>
2020-09-19 15:36:21 -0400

    add screens and navigation

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import 'react-native-gesture-handler';
-import React, {Component} from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'; 
 
 // Screen Imports will come here 
-import Home from './screens/Home'; 
+import Home from './screens/Home';
+import Signup from './screens/Signup';
+import Login from './screens/Login';
+import Dashboard from './screens/Dashboard';
+ 
 
 // initializes Stack Navigator to allow us to move between different screens
 const Stack = createStackNavigator(); 
@@ -19,6 +19,9 @@ const Stack = createStackNavigator();
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name ="Home" component ={Home} />
+          <Stack.Screen name ="Signup" component ={Signup} />
+          <Stack.Screen name ="Login" component ={Login} />
+          <Stack.Screen name ="Dashboard" component ={Dashboard} />
         </Stack.Navigator>
       </NavigationContainer>
     )
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'; 
-import {View, Text, StyleSheet} from 'react-native'; 
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'; 
 
 
 export default class Home extends Component {
@@ -7,6 +7,12 @@ export default class Home extends Component {
         return (
             <View style={styles.container}>
                 <Text>Welcome to SpotMe</Text>
+                <TouchableOpacity onPress = {() => this.props.navigation.navigate("Signup")}>
+                    <Text>Signup</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress = {() => this.props.navigation.navigate("Login")}>
+                    <Text>Already have an account? Login</Text>
+                </TouchableOpacity>
             </View>
         )
     }
