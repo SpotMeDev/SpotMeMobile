58c7abf69e7ff2467b9deaabd50f0df78d6deb21
Gagandeep Kang <gskang@bu.edu>
2020-09-19 11:46:41 -0400

    initialize application and add stack navigator

---
+++ App.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler';
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'; 
+
+// Screen Imports will come here 
+import Home from './screens/Home'; 
+
+// initializes Stack Navigator to allow us to move between different screens
+const Stack = createStackNavigator(); 
+
+
+ export default function App() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name ="Home" component ={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+}
