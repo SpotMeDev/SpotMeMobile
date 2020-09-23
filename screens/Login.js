a4d4b1f963e5480f0e72fd76f7c7f4dcb636bd22
Gagandeep Kang <gskang@bu.edu>
2020-09-23 14:41:26 -0400

    add signup and login functionality

--- screens/Login.js
+++ screens/Login.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react'; 
-import {View, Text, StyleSheet} from 'react-native'; 
+import {View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'; 
+import axios from 'axios'; 
 
 
 
 export default class Login extends Component {
+    state = {
+        email: '', 
+        password: ''
+    }
+
+    handleLoginForm = (type, newValue) => {
+        switch (type) {
+            case 'email': 
+                this.setState({email: newValue})
+                break; 
+            case 'password': 
+                this.setState({password: newValue})
+                break; 
+        }
+    }
+
+    login = () => {
+        if (this.state.email != "" &&  this.state.password != "") {
+            axios.post("http://localhost:8080/auth/login", {email: this.state.email, password: this.state.password}).then(resp => {
+                // deal with JWT token 
+                console.log(resp)
+                this.props.navigation.navigate('Dashboard'); 
+            }).catch(err => {
+                console.log("error", err); 
+            })
+        }
+    }
+
     render(){
         return (
             <View style = {styles.container}>
                 <Text>Welcome to the Login Screen!</Text>
+                <TextInput placeholder = {"email"} value = {this.state.email}  onChangeText = {text => this.handleLoginForm('email', text)} /> 
+                <TextInput placeholder = {"password"} value = {this.state.password}  onChangeText = {text => this.handleLoginForm('password', text)} secureTextEntry={true}/> 
+                <TouchableOpacity onPress = {this.login}>
+                    <Text>Login</Text>
+                </TouchableOpacity>
             </View>
         )
     }
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,13 +1,56 @@
 import React, {Component} from 'react'; 
-import {View, Text, StyleSheet} from 'react-native'; 
-
+import {View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'; 
+import axios from 'axios'; 
 
 
 export default class Signup extends Component {
+    state = {
+        name: '', 
+        email: '', 
+        password: '', 
+        confirmPassword: ''
+    }
+
+    handleFormChange = (type, newValue) => { 
+        switch (type) {
+            case "name": 
+                this.setState({name: newValue})
+                break; 
+            case "email": 
+                this.setState({email: newValue})  
+                break;           
+            case "password": 
+                this.setState({password: newValue})
+                break;           
+            case "confirmPassword": 
+                this.setState({confirmPassword: newValue})
+                break; 
+        }
+    }
+
+    signupUser = () => {
+        if (this.state.name != "" && this.state.email != "" &&  this.state.password != "" &&  this.state.confirmPassword != "") {
+            axios.post("http://localhost:8080/auth/signup", {name: this.state.name, email: this.state.email, password: this.state.password, confirmPassword: this.state.confirmPassword}).then(resp => {
+                // deal with JWT token 
+                console.log(resp)
+                this.props.navigation.navigate('Dashboard'); 
+            }).catch(err => {
+                console.log("error", err); 
+            })
+        }
+    }
+
     render(){
         return (
             <View style = {styles.container}>
                 <Text>Welcome to the Signup Screen!</Text>
+                <TextInput placeholder = {"Name"} value = {this.state.name} onChangeText={text => this.handleFormChange('name', text)} />
+                <TextInput placeholder = {"Email"} value = {this.state.email} onChangeText={text => this.handleFormChange('email', text)} />
+                <TextInput placeholder = {"Password"} value = {this.state.password} onChangeText={text => this.handleFormChange('password', text)} secureTextEntry={true}/>
+                <TextInput placeholder = {"Confirm Password"} value = {this.state.confirmPassword} onChangeText={text => this.handleFormChange('confirmPassword', text)} secureTextEntry={true} />
+                <TouchableOpacity onPress = {() => this.signupUser()}>
+                    <Text>Submit</Text>
+                </TouchableOpacity>
             </View>
         )
     }
